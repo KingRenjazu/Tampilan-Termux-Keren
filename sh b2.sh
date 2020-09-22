590dc4e355676409a466328c5596140f7c664c78
KingRenjazu <71654446+yovan081@users.noreply.github.com>
2020-09-22 08:22:14 +0700

    Add files via upload

---
+++ sh b2.sh
@@ -0,0 +1,34 @@
+black='\e[0;30m'
+blue='\e[0;34m'
+green='\e[0;32m'
+cyan='\e[0;36m'
+red='\e[0;31m'
+purple='\e[0;35m'
+brown='\e[0;33m'
+lightgray='\e[0;37m'
+darkgray='\e[1;30m'
+lightblue='\e[1;34m'
+lightgreen='\e[1;32m'
+lightcyan='\e[1;36m'
+lightred='\e[1;31m'
+lightpurple='\e[1;35m'
+yellow='\e[1;33m'
+white='\e[1;37m'
+nc='\e[0m'
+echo.
+echo"*🔰Indonesian CYBER ARMY*🔰------------------------------------------
+*Author : KingRenjazu*
+*My Team : Indonesian Cyber Army*
+*Contack Us : 081387266786*
+------------------------------------------------
+*🔰SELAMAT MENCOBA*🔰"
+___________
+< WhatsApp >
+ ---------
+╲╲╭━━━━╮
+╭╮┃▆┈┈▆┃╭╮  root@king_renjazu
+┃╰┫▽▽▽┣╯┃
+╰━┫△△△┣━╯
+╲╲┃┈┈┈┈┃
+╲╲┃┈┏┓┈┃
+▔▔╰━╯╰━╯
